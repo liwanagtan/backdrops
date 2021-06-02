1654e29c0298c9110ef68d550b33fefb758de288
Jonathan Liwanag <jonathanulandayliwanag@gmail.com>
2021-06-02 21:34:25 +0800

    Udpated json

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,3 @@
-<!-- <script context="module">
-    import { loadNav } from './_strapi';
-    export async function load(loadOptions) {
-      return await loadNav(loadOptions);
-    };
-</script> -->
-
 <script>
     import '../app.css';
 </script>
